28534501e4cc5a5c8550c18709f8a1749b205554
JoseZapatah <72279136+JoseZapatah@users.noreply.github.com>
2020-12-09 09:22:32 -0300

    Fix error

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Home2(),
         theme: ThemeData(
           primaryColor: Colors.lightBlue,
