fe9eb661a38accf523a44b0cba2feff8757ffda7
JoseZapatah <72279136+JoseZapatah@users.noreply.github.com>
2020-12-19 19:48:06 -0300

    Script de python
    Agregada la funcion de enviar notificaciones push.
    Agregada permiso de administrador.

---
+++ requestApi/SendPush.py
@@ -0,0 +1,4 @@
+import main as fcm
+
+tokens = ["egNERmfIh7I:APA91bGIoWOL3daGWQjMAgqkh-s00943TJyfxoDCSY33F-Mrcr6WhlYOnu6s7Z0H21Cy_fmkj_xYMv_LF0zmesvu6fMIoBEBZ91ytoREIAvzA8BAYGOonoSbZepGhxFFiXpi8eBxE_Zw"]
+fcm.sendPush("Hi", "This is my next msg")
